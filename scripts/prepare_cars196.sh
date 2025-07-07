56e4b6f94fc364b07ef73e0ebcaa06d79db9f85b
Thành Hưng <hungnguyenarbeit@gmail.com>
2025-07-07 20:10:53 +0700

    Update prepare_cars196.sh

--- scripts/prepare_cars196.sh
+++ scripts/prepare_cars196.sh
@@ -3,15 +3,15 @@ set -e
 
 
 CARS196_ROOT='data/cars196/'
-CARS196_DATA='1se8UcOW1hu752bXCw4GyYzkJkCO4Kt6Z'
+CARS196_DATA='199pRuHUlDdtnvOQm-drmj-20P4GrN43T'
 
 if [[ ! -d "${CARS196_ROOT}" ]]; then
     mkdir -p data/
     pushd data/
     echo "Downloading Cars196 data-set..."
-    gdown "${CARS196_DATA}" -O cars196.zip
-    unzip cars196.zip
-    rm cars196.zip
+    gdown "${CARS196_DATA}" -O cars196.tar
+    tar -xvf cars196.tar
+    rm cars196.tar
     popd
 fi
 
