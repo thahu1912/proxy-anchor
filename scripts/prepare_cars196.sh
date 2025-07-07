1f691968bea3436e27b8333cf9cdc98728f4f3f8
Thành Hưng <hungnguyenarbeit@gmail.com>
2025-07-07 15:31:16 +0700

    fix cars196 datase

--- scripts/prepare_cars196.sh
+++ scripts/prepare_cars196.sh
@@ -4,7 +4,7 @@ set -e
 
 CARS196_ROOT='data/cars196/'
 CARS196_DATA='https://www.dropbox.com/scl/fi/0a7bi36c55qb9bhm4pzzu/cars196.tar?rlkey=8rld9uelq39iw8r5moootq19g&dl=0'
-CARS196_DEVKIT='1t2CzZy3oEpZnb7vPwp68Z7AwyI43osuS'
+CARS196_DEVKIT='1rdETvCItDkiWecqL_L2F_qQOSNUcmdSd'
 
 
 if [[ ! -d "${CARS196_ROOT}" ]]; then
@@ -12,11 +12,11 @@ if [[ ! -d "${CARS196_ROOT}" ]]; then
     pushd data/
     echo "Downloading Cars196 data-set..."
     wget -O cars196.tar "${CARS196_DATA}"
-    gdown "${CARS196_DEVKIT}" -O cars_devkit.tgz
+    gdown "${CARS196_DEVKIT}" -O cars_annos.zip
     tar -xvf cars196.tar
-    tar -xvf cars_devkit.tgz
+    unzip cars_annos.zip
     rm cars196.tar
-    rm cars_devkit.tgz
+    rm cars_annos.zip
 
     popd
 fi
