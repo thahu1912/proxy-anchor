dd2c7ca15faac520f2c5dff5d9043b7b005a5589
Thành Hưng <hungnguyenarbeit@gmail.com>
2025-07-10 08:14:51 +0700

    Update table1.sh

--- code/table1.sh
+++ code/table1.sh
@@ -28,32 +28,7 @@ python train.py --gpu-id 0 \
                 --lr-decay-step 10
 
 
-###################ResNet50####################
-python train.py --gpu-id 0 \
-                --loss Uncertainty_Aware_Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset cub \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
-                --variance-weight 0.1 \
-                --hyper-weight 0.2 \
 
-
-
-python train.py --gpu-id 0 \
-                --loss Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset cub \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
 
 ###################GoogleNet####################
 python train.py --gpu-id 0 \
@@ -110,33 +85,6 @@ python train.py --gpu-id 0 \
                 --bn-freeze 1 \
                 --lr-decay-step 10
 
-
-###################ResNet50####################
-python train.py --gpu-id 0 \
-                --loss Uncertainty_Aware_Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset cars \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
-                --variance-weight 0.1 \
-                --hyper-weight 0.2 \
-
-
-
-python train.py --gpu-id 0 \
-                --loss Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset cars \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
 
 ###################GoogleNet####################
 python train.py --gpu-id 0 \
@@ -195,32 +143,6 @@ python train.py --gpu-id 0 \
                 --lr-decay-step 10
 
 
-###################ResNet50####################
-python train.py --gpu-id 0 \
-                --loss Uncertainty_Aware_Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset SOP \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
-                --variance-weight 0.1 \
-                --hyper-weight 0.2 \
-
-
-
-python train.py --gpu-id 0 \
-                --loss Proxy_Anchor \
-                --model resnet50 \
-                --embedding-size 512 \
-                --batch-size 180 \
-                --lr 1e-4 \
-                --dataset cars \
-                --warm 1 \
-                --bn-freeze 1 \
-                --lr-decay-step 10
 
 ###################GoogleNet####################
 python train.py --gpu-id 0 \
